8c5b651cbf0590a7a3525f16422a443890c5f893
Saurabh <saurabh.student.met15@itbhu.ac.in>
2018-12-15 06:03:34 +0530

    Lets try

---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+
+from . import views
+# To add a new path, first import the app:
+# import blog
+#
+# Then add the new path:
+# path('blog/', blog.urls, name="blog")
+#
+# Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
+
+urlpatterns = [
+	path("1/", views.index, name="index"),
+]
---
+++ gettingstarted/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path, include
+
+from django.contrib import admin
+
+admin.autodiscover()
+
+import hello
+import blog
+
+# To add a new path, first import the app:
+# import blog
+#
+# Then add the new path:
+# path('blog/', blog.urls, name="blog")
+#
+# Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
+
+urlpatterns = [
+	path("", include('blog.urls')),
+	path("start/", include('hello.urls')),
+#	path("", bv.index, name="index"),
+    
+#    path("db/", include(hello.urls)),
+    path("admin/", admin.site.urls),
+]
